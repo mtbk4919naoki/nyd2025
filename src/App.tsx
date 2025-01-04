f3496310eb6694e969bfe8ffe7d18d9dbdd5189e
Naoki Yamamoto <mtbk4919naoki@gmail.com>
2025-01-05 00:59:45 +0900

    update: bugfix

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 const BOARD_SIZE = 10;
 const INITIAL_SNAKE = [{ x: 2, y: 2 }];
 const INITIAL_DIRECTION = { x: 0, y: 1 };
+const DELAY_TIME = 200;
 
 function App() {
   const [snake, setSnake] = useState(INITIAL_SNAKE);
@@ -11,20 +12,32 @@ function App() {
   const [food, setFood] = useState({ x: 5, y: 5 });
   const [gameOver, setGameOver] = useState(false);
   const [gameStarted, setGameStarted] = useState(false);
-  const [turn, setTurn] = useState(false);
+  const [turn, setTurn] = useState('ahead');
+  const [startTime, setStartTime] = useState(0);
+  const [isKeydown, setIsKeydown] = useState(false);
+  const [directionQueue, setDirectionQueue] = useState('ahead');
+  const generateFoodPosition = (snake: { x: number, y: number }[]) => {
+    let newFoodPosition: { x: number, y: number };
+    do {
+      newFoodPosition = {
+        x: Math.floor(Math.random() * BOARD_SIZE),
+        y: Math.floor(Math.random() * BOARD_SIZE),
+      };
+    } while (snake.some(segment => segment.x === newFoodPosition.x && segment.y === newFoodPosition.y));
+    return newFoodPosition;
+  };
 
   const startGame = () => {
     setGameStarted(true);
     setGameOver(false);
     setSnake(INITIAL_SNAKE);
     setDirection(INITIAL_DIRECTION);
-    setFood({ x: 5, y: 5 });
+    setFood(generateFoodPosition(INITIAL_SNAKE));
   };
 
   const moveSnake = useCallback(() => {
     if (!gameStarted) return;
     setSnake(prevSnake => {
-      setTurn(false);
       const newHead = {
         x: prevSnake[0].x + direction.x,
         y: prevSnake[0].y + direction.y,
@@ -33,7 +46,7 @@ function App() {
       if (
         newHead.x < 0 || newHead.x >= BOARD_SIZE ||
         newHead.y < 0 || newHead.y >= BOARD_SIZE ||
-        prevSnake.some(segment => segment.x === newHead.x && segment.y === newHead.y)
+        prevSnake.slice(0, -1).some(segment => segment.x === newHead.x && segment.y === newHead.y)
       ) {
         setGameOver(true);
         return prevSnake;
@@ -42,10 +55,11 @@ function App() {
       let newSnake;
       if (newHead.x === food.x && newHead.y === food.y) {
         newSnake = [newHead, ...prevSnake];
-        setFood({
-          x: Math.floor(Math.random() * BOARD_SIZE),
-          y: Math.floor(Math.random() * BOARD_SIZE),
-        });
+        if (newSnake.length === BOARD_SIZE * BOARD_SIZE) {
+          setGameOver(true);
+        } else {
+          setFood(generateFoodPosition(newSnake));
+        }
       } else {
         newSnake = [newHead, ...prevSnake.slice(0, -1)];
       }
@@ -55,17 +69,24 @@ function App() {
   }, [direction, food, gameStarted, turn]);
 
   const changeDirection = (e: KeyboardEvent) => {
+    setIsKeydown(true);
     setDirection(prevDirection => {
       switch (e.key) {
         case 'ArrowLeft':
-          if (!turn) {
-            setTurn(true);
+          if (turn !== 'ahead') {
+            setDirectionQueue('left');
+          }
+          if (turn !== 'left') {
+            setTurn('left');
             return { x: -prevDirection.y, y: prevDirection.x };
           }
           return { x: prevDirection.x, y: prevDirection.y }
         case 'ArrowRight':
-          if (!turn) {
-            setTurn(true);
+          if (turn !== 'ahead') {
+            setDirectionQueue('right');
+          }
+          if (turn !== 'right') {
+            setTurn('right');
             return { x: prevDirection.y, y: -prevDirection.x };
           }
           return { x: prevDirection.x, y: prevDirection.y }
@@ -76,6 +97,7 @@ function App() {
   };
 
   const handleSwipe = useCallback((startX: number, startY: number, endX: number, endY: number) => {
+    setIsKeydown(false);
     const diffX = endX - startX;
     const diffY = endY - startY;
 
@@ -83,13 +105,25 @@ function App() {
       // Horizontal swipe
       if (diffX > 0) {
         // Swipe right
-        setDirection({ x: direction.y, y: -direction.x });
+        if (turn !== 'ahead') {
+          setDirectionQueue('right');
+        }
+        if (turn !== 'right') {
+          setTurn('right');
+          setDirection({ x: direction.y, y: -direction.x });
+        }
       } else {
         // Swipe left
-        setDirection({ x: -direction.y, y: direction.x });
+        if (turn !== 'ahead') {
+          setDirectionQueue('left');
+        }
+        if (turn !== 'left') {
+          setTurn('left');
+          setDirection({ x: -direction.y, y: direction.x });
+        }
       }
     }
-  }, [direction]);
+  }, [direction, turn, directionQueue]);
 
   useEffect(() => {
     let startX: number;
@@ -119,7 +153,31 @@ function App() {
 
   useEffect(() => {
     if (gameOver || !gameStarted) return;
-    const interval = setInterval(moveSnake, 200);
+    let timeLeft = DELAY_TIME;
+    if (isKeydown) {
+      // キーボード操作の場合は操作遅延を無効にする
+      timeLeft = Math.min(DELAY_TIME - (performance.now() - startTime) / 2, DELAY_TIME);
+      if (timeLeft < 1) {
+        timeLeft = DELAY_TIME;
+      }
+    }
+
+    const interval = setInterval(() => {
+      setTurn('ahead');
+      moveSnake();
+      if (directionQueue !== 'ahead') {
+        if (directionQueue === 'left') {
+          setTurn('left');
+          setDirection({ x: -direction.y, y: direction.x });
+        } else if (directionQueue === 'right') {
+          setTurn('right');
+          setDirection({ x: direction.y, y: -direction.x });
+        }
+        setDirectionQueue('ahead');
+      }
+      setStartTime(performance.now());
+
+    }, timeLeft);
     document.addEventListener('keydown', changeDirection);
     return () => {
       clearInterval(interval);
@@ -129,14 +187,16 @@ function App() {
 
   return (
     <>
-      <h1>スネークゲーム</h1>
-      <p>蛇を動かして、お餅をたくさん食べよう！</p>
+      <h1 className="title">スネークゲーム</h1>
+      <p>蛇を動かしてお餅をたくさん食べよう！</p>
       <div>
-        <h2>操作説明</h2>
+        <h2 className="subtitle">操作説明</h2>
         <p>キーボードの左右キーで操作します。<br />左右スワイプでも操作できます。</p>
       </div>
       {!gameStarted ? (
-        <button onClick={startGame}>はじめる</button>
+        <div className="board" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <button onClick={startGame}>はじめる</button>
+        </div>
       ) : (
         <div className="board">
           {gameOver ? (
@@ -152,7 +212,9 @@ function App() {
                   <div
                     key={col}
                     className={`cell ${snake.some(segment => segment.x === col && segment.y === row)
-                      ? 'snake'
+                      ? (col === snake[0].x && row === snake[0].y)
+                        ? 'snake-head'
+                        : 'snake'
                       : food.x === col && food.y === row
                         ? 'food'
                         : ''
